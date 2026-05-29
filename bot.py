a57b17088feeb725ba4cb22c5a9e52e23cf06a18
marcocassano88-byte <marco.cassano88@gmail.com>
2026-05-29 10:20:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,22 +3,15 @@ import time
 import hashlib
 import feedparser
 from telegram import Bot
-from deep_translator import GoogleTranslator
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 bot = Bot(token=BOT_TOKEN)
 
-RSS_FEED = "https://www.animenewsnetwork.com/all/rss.xml"
+RSS_FEED = "https://news.google.com/rss/search?q=One+Piece+anime&hl=it&gl=IT&ceid=IT:it"
 
 posted = set()
-
-def translate(text):
-    try:
-        return GoogleTranslator(source='auto', target='it').translate(text)
-    except:
-        return text
 
 def make_id(text):
     return hashlib.md5(text.encode()).hexdigest()
@@ -31,35 +24,17 @@ while True:
         link = entry.link
 
         uid = make_id(title)
+
         if uid in posted:
             continue
 
         posted.add(uid)
 
-        title_it = translate(title)
-
-        # 🔥 immagine automatica dal feed (OG IMAGE se esiste)
-        image = None
-
-        if "media_content" in entry:
-            try:
-                image = entry.media_content[0]["url"]
-            except:
-                pass
-
-        if not image:
-            image = "https://i.imgur.com/8Km9tLL.jpg"  # fallback
-
-        caption = f"🔥 {title_it}\n\n👉 Fonte: {link}"
+        message = f"🔥 {title}\n\n👉 Fonte: {link}"
 
         try:
-            bot.send_photo(
-                chat_id=CHAT_ID,
-                photo=image,
-                caption=caption
-            )
-
-            print("POST:", title_it)
+            bot.send_message(chat_id=CHAT_ID, text=message)
+            print("POST:", title)
 
         except Exception as e:
             print("Errore:", e)
