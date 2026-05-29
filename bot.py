1400eb078c0c2b0337040d9dd4e830d8c759f627
marcocassano88-byte <marco.cassano88@gmail.com>
2026-05-29 09:29:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,12 @@
-import telegram
-import os
-import time
+from telegram import Bot
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+BOT_TOKEN = "INSERISCI_TOKEN"
+CHAT_ID = "@nomecanale"
 
-bot = telegram.Bot(token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN)
 
-posts = [
-    "🔥 One Piece: nuove teorie su Imu e il Secolo Vuoto!\n\n#onepiece #anime",
-    "🏴‍☠️ Eiichiro Oda anticipa eventi clamorosi del finale di One Piece.\n\n#luffy #onepiece",
-    "⚔️ Zoro continua a essere uno dei personaggi più amati della serie.\n\n#zoro #onepiece",
-    "🔥 Gear 5 continua a dominare il fandom anime mondiale.\n\n#gear5 #luffy",
-    "👑 Shanks potrebbe conoscere il vero tesoro One Piece.\n\n#shanks #onepiece",
-]
-
-for post in posts:
-
-    try:
-        bot.send_message(
-            chat_id=CHAT_ID,
-            text=post
-        )
-
-        print("Post inviato")
-
-        time.sleep(20)
-
-    except Exception as e:
-        print(e)
+try:
+    bot.send_message(chat_id=CHAT_ID, text="TEST BOT OK ✅")
+    print("FUNZIONA")
+except Exception as e:
+    print("ERRORE:", e)
